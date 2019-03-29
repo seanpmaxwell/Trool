bd359b515d209b6a4597fd779bfa6f5f71fb3676
seanpmaxwell <seanpmaxwell1@gmail.com>
2019-03-28 21:30:09 -0700

    doc cont

--- sample-project-2/src/CalcEngine.ts
+++ sample-project-2/src/CalcEngine.ts
@@ -8,6 +8,9 @@
 
 class CalcEngine {
 
+    // constructor() {
+    //
+    // }
 
 }
 
--- sample-project-2/src/models/Patient.ts
+++ sample-project-2/src/models/Patient.ts
@@ -4,14 +4,18 @@
  * created by Sean Maxwell Mar 27, 2019
  */
 
+type ageGroups = 'Adult' | 'Child' | 'Senior' | null;
+
 
 class Patient {
 
     private _age: number;
+    private _ageGroup: ageGroups;
 
 
     constructor() {
         this._age = -1;
+        this._ageGroup = null;
     }
 
     set age(age: number) {
@@ -22,8 +26,12 @@ class Patient {
         return this._age;
     }
 
-    set patientType(): {
+    set ageGroup(ageGroup: ageGroups) {
+        this._ageGroup = this.ageGroup;
+    }
 
+    get ageGroup(): ageGroups {
+        return this._ageGroup;
     }
 }
 
